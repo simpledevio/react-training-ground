089bbddb98f1825f4675e61aa6dc2a436819382f
Javier Alvarado <jdrummer1@outlook.com>
2025-05-13 23:24:35 -0700

    Add HelloWorld.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css'
+import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
-
-  return <h1>Hello World</h1>;
+  return <HelloWorld />;
 }
 
-export default App
+export default App;
